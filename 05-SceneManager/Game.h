25ed7de32d4bba00a8b5ccae315c4c68a75c4c3b
krypton99 <32381540+krypton99@users.noreply.github.com>
2022-04-27 19:13:04 +0700

    Add function Get, Set Camera X and Y

--- 05-SceneManager/Game.h
+++ 05-SceneManager/Game.h
@@ -105,7 +105,10 @@ public:
 
 	void SetCamPos(float x, float y) { cam_x = x; cam_y = y; }
 	void GetCamPos(float& x, float& y) { x = cam_x; y = cam_y; }
-
+	void SetCamPosX(float x) { cam_x = x; }
+	float GetCamPosX() { return cam_x; }
+	void SetCamPosY(float y) { cam_y = y; }
+	float GetCamPosY() {return cam_y; }
 	LPSCENE GetCurrentScene() { return scenes[current_scene]; }
 	void Load(LPCWSTR gameFile);
 	void SwitchScene();
