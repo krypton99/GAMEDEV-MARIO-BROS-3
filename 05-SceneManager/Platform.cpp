a57ddfa6df7e7c546a143e75b6f1a7e39047f3bd
krypton99 <32381540+krypton99@users.noreply.github.com>
2022-05-01 18:38:26 +0700

    platform did't have to render itself because tile background had already rendered

--- 05-SceneManager/Platform.cpp
+++ 05-SceneManager/Platform.cpp
@@ -30,21 +30,7 @@ void CPlatform::RenderBoundingBox()
 
 void CPlatform::Render()
 {
-	if (this->length <= 0) return; 
-	float xx = x; 
-	CSprites * s = CSprites::GetInstance();
-
-	s->Get(this->spriteIdBegin)->Draw(xx, y);
-	xx += this->cellWidth;
-	for (int i = 1; i < this->length - 1; i++)
-	{
-		s->Get(this->spriteIdMiddle)->Draw(xx, y);
-		xx += this->cellWidth;
-	}
-	if (length>1)
-		s->Get(this->spriteIdEnd)->Draw(xx, y);
-
-	RenderBoundingBox();
+	
 }
 
 void CPlatform::GetBoundingBox(float& l, float& t, float& r, float& b)
--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -127,14 +127,10 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 		float cell_width = (float)atof(tokens[3].c_str());
 		float cell_height = (float)atof(tokens[4].c_str());
 		int length = atoi(tokens[5].c_str());
-		int sprite_begin = atoi(tokens[6].c_str());
-		int sprite_middle = atoi(tokens[7].c_str());
-		int sprite_end = atoi(tokens[8].c_str());
 
 		obj = new CPlatform(
 			x, y,
-			cell_width, cell_height, length,
-			sprite_begin, sprite_middle, sprite_end
+			cell_width, cell_height, length
 		);
 
 		break;
