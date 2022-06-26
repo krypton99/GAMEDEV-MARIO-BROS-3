ca164918dbddc79473030a301a3505bfc6b5eeb3
krypton99 <17520777@gm.uit.edu.vn>
2022-06-26 17:55:57 +0700

    render score number in scoreboard

--- 05-SceneManager/HUD.cpp
+++ 05-SceneManager/HUD.cpp
@@ -1,20 +1,36 @@
 #include "HUD.h"
 #include "Camera.h"
+#include <stdlib.h>
+
+char* IntToChar(int value, int length)
+{
+	char* c = new char[length + 1];
+	c[length] = '\0';
+	for (int i = length - 1; i >= 0; i--)
+	{
+		c[i] = value % 10 + 48;
+		value = value / 10;
+	}
+	return c;
+}
 
 void HUD::Render(float x, float y, CMario* mario) {
 	float vx, vy;
 	mario->GetSpeed(vx, vy);
 	board = CAnimations::GetInstance();
 	background = CAnimations::GetInstance();
+
+	
 	if (mario->GetStage() == WORLD_MAP_SCENE) {
-		speedBar = new CSpeedBar(x + 92, y + SCREEN_HEIGHT / 2 + 60);
-		background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH / 2, y + SCREEN_HEIGHT / 2 + 90);
-		board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH / 2 + 8, y + SCREEN_HEIGHT / 2 + 64);
+		speedBar = new CSpeedBar(x + 92, y + SCREEN_HEIGHT / 2 + SPEEDBAR__OFFSET+WORLDMAP_OFFSET);
+		background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH / 2, y + SCREEN_HEIGHT / 2 + BACKGROUND_OFFSET + WORLDMAP_OFFSET);
+		board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH / 2 + 8, y + SCREEN_HEIGHT / 2 + BOARD_OFFSET + WORLDMAP_OFFSET);
 	}
 	else {
-		speedBar = new CSpeedBar(x + 92, y + SCREEN_HEIGHT / 2 + 32);
-		background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH / 2, y + SCREEN_HEIGHT / 2 + 64);
-		board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH / 2 + 8, y + SCREEN_HEIGHT / 2 + 36);
+		speedBar = new CSpeedBar(x + 92, y + SCREEN_HEIGHT / 2 + SPEEDBAR__OFFSET);
+		background->Get(ID_ANI_BACKGROUND)->Render(x + SCREEN_WIDTH / 2, y + SCREEN_HEIGHT / 2 + BACKGROUND_OFFSET);
+		board->Get(ID_ANI_BOARD)->Render(x + BOARD_WIDTH / 2 + 8, y + SCREEN_HEIGHT / 2 + BOARD_OFFSET);
+		text.Render(IntToChar(mario->getScore(),7),  x + time_margin_x, y + money_margin_y );
 	}
 	
 	speedBar->Render(vx,mario->GetIsPowerUp());
--- 05-SceneManager/HUD.h
+++ 05-SceneManager/HUD.h
@@ -2,6 +2,7 @@
 #include "Game.h"
 #include "Mario.h"
 #include "SpeedBar.h"
+#include "Text.h"
 #define ID_ANI_BOARD 9901
 #define ID_ANI_BACKGROUND 9999
 
@@ -10,12 +11,21 @@
 
 #define BOARD_LEFT_MARGIN 8
 
+#define BACKGROUND_OFFSET 64
+#define BOARD_OFFSET 36
+#define SPEEDBAR__OFFSET 32
 
+#define WORLDMAP_OFFSET 28
+
+#define money_margin_y		160
+
+#define time_margin_x		64
 class HUD
 {
 	CSpeedBar* speedBar = nullptr;
 	CAnimations* board = nullptr;
 	CAnimations* background = nullptr;
+	Text text;
 public:
 	HUD() {};
 
